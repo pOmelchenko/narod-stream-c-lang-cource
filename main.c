72117da30fc389eb0ec91bda09d8bf5de53a237d
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 01:21:52 +0300

    add printing other types in output

--- main.c
+++ main.c
@@ -8,5 +8,31 @@ int main()
     printf("Char: ");
     printf("%c\r\n", 97);
 
+    printf("Signed integer: ");
+    printf("%d\r\n", 3000000000);
+
+    printf("Unsigned integer: ");
+    printf("%u\r\n", 3000000000);
+
+    printf("Signed short: ");
+    printf("%hd\r\n", 40000);
+
+    printf("Unsigned short: ");
+    printf("%hu\r\n", 40000);
+
+    printf("Signed long: ");
+    printf("%lli\r\n", 18446744073709551615);
+
+    printf("Unsigned long: ");
+    printf("%llu\r\n", 18446744073709551615);
+
+    printf("Float: ");
+    printf("%f\r\n", 12.456f);
+    printf("Double: ");
+    printf("%lf\r\n", 12345.67891);
+
+    printf("Hex: ");
+    printf("0x%llX\r\n", 3000000000);
+
     return 0;
 }
