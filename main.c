8c0751753e87bcf093a87357ef31bba77dd08ffa
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 13:44:40 +0300

    result of the lesson 4.2

--- main.c
+++ main.c
@@ -6,43 +6,54 @@
 
 int main()
 {
-    // вывод строки
-    printf("String: ");
-    printf("%s\r\n", "Hello world!!!");
+    char c1, c2;
+    unsigned char c3;
+    short ns1;
+    unsigned short ns2;
+    int ni1;
+    unsigned ni2;
+    long nl1;
+    unsigned long nl2;
+    long long nll1;
+    unsigned long long nll2;
+    float f;
+    double d;
 
-    // вывод символа
-    printf("Char: ");
-    printf("%c\r\n", 97);
+    c1 = 'r';
+    c2 = -115;
+    c3 = 195;
+    ns1 = -31511;
+    ns2 = 63699;
+    ni1 = -2011242517;
+    ni2 = 4000000000;
+    nl1 = -2011242517;
+    nl2 = 4000000000;
+    nll1 = -8446744073709551615;
+    nll2 = 18446744073709551234ULL;
+    f = 12.456f;
+    d = 1.5364e-20;
 
-#if 0
-    printf("Signed integer: ");
-    printf("%d\r\n", 3000000000);
+    // output
+    printf("Char symbol: %c\n", c1);
+    printf("Signed char: %hd\n", c2);
+    printf("Unsigned char: %hd\n", c3);
 
-    printf("Unsigned integer: ");
-    printf("%u\r\n", 3000000000);
-#endif
+    printf("Signed short: %hd\n", ns1);
+    printf("Unsigned short: %hu\n", ns2);
 
-    // вывод коротких чисел
-    printf("Signed short: ");
-    printf("%hd\r\n", 40000);
-    printf("Unsigned short: ");
-    printf("%hu\r\n", 40000);
+    printf("Signed integer: %d\n", ni1);
+    printf("Unigned integer: %u\n", ni2);
 
-    // вывод длинных чисел
-    printf("Signed long: ");
-    printf("%lli\r\n", 18446744073709551615);
-    printf("Unsigned long: ");
-    printf("%llu\r\n", 18446744073709551615);
+    printf("Signed long: %ld\n", nl1);
+    printf("Unsigned long: %lu\n", nl2);
 
-    // вывод чисел с плавающей точкой
-    printf("Float: ");
-    printf("%f\r\n", 12.456f);
-    printf("Double: ");
-    printf("%lf\r\n", 12345.67891);
+    printf("Signed long long: %lld\n", nll1);
+    printf("Unsigned long long: %llu\n", nll2);
 
-    // вывод в hex
-    printf("Hex: ");
-    printf("0x%llX\r\n", 3000000000);
+    printf("Float: %f\n", f);
+    printf("Double: %.24lf\n", d);
+
+    printf("Hex: 0x%X\n", ni2);
 
     return 0;
 }
