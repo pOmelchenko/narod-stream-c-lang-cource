c1a7f29cd644a12fc603c82adad7755cb90a6710
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 20:19:13 +0300

    try to work with string arrays

--- main.c
+++ main.c
@@ -2,11 +2,21 @@
 
 int main() {
     unsigned char i;
-    int array[6] = {18, -32, 143};
+    char string[10] = {'H', 'e', 'l', 'l', 'o', '!', '\0'};
 
-    for (i = 0; i < 6; i++) {
-        printf("%d ", array[i]);
+    for (i = 0; i < 10; i++) {
+        printf("%d ", string[i]);
     }
+
+    printf("\n\n");
+
+    for (i = 0; i < 10; i++) {
+        printf("%c ", string[i]);
+    }
+
+    printf("\n\n");
+
+    printf("%s", string);
 
     return 0;
 }
