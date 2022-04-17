03702dfeb7361f14d4e28e90b660a07c8776314c
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 20:10:13 +0300

    second try to work with arrays

--- main.c
+++ main.c
@@ -2,15 +2,8 @@
 
 int main() {
     unsigned char i;
-    int array[6];
-
-    array[0] = 10;
-    array[1] = 25;
-    array[2] = 33;
-    array[3] = 71;
-    array[4] = 54;
-    array[5] = 99;
-
+    int array[6] = {10, 25, 33, 71, 54, 99};
+    
     for (i = 0; i < 6; i++) {
         printf("%d ", array[i]);
     }
