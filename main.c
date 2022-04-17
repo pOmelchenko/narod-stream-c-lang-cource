4246dea12f5ddddc96bd3008b3ca3748978d86c5
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 16:31:00 +0300

    work with loops

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    /**
+     * while loop
+     */
+    int i = 10;
+    while (i >= 0) {
+        printf("%d\n", i);
+        i--;
+    }
 
+    printf("========================\n");
+
+    /**
+     * do-while loop
+     */
+    i = 10;
+    do {
+        printf("%d\n", i);
+        i--;
+    } while (i >= 0);
+
+    printf("========================\n");
+
+    /**
+     * for loop
+     */
+    for (i=0; i<=10; i++) {
+        printf("%d\n", i);
+    }
 
     return 0;
 }
