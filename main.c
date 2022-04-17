4a21e901e86e6518718b870f96a5bf021c387713
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-18 00:59:45 +0300

    continue work with array strings

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char strings[3][30];
-
-    strcpy(strings[0], "Hello world!\n");
-    strcpy(strings[1], "Hi!\n");
-    strcpy(strings[2], "Pavel!\n");
+    char strings[3][32] = {
+        "Hello world!\n",
+        "Hi!\n",
+        "Pavel!\n"
+    };
 
     printf("%s%s%s", strings[0], strings[1], strings[2]);
 
