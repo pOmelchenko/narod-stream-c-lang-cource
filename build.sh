788640e7dc074fe23b869e354bdb4c49cf01524d
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 13:45:14 +0300

    update build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 gcc -Wall -E main.c -o main.i
-gcc -Wall -S main.i -o main.s
-gcc -Wall -c main.c
+gcc -masm=intel -g3 -Wall -S main.i -o main.s
+gcc -g3 -Wall -c main.c
 gcc -Wall main.o -o main
