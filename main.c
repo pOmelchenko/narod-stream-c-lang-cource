be2a01a9bdb5ea8f350526ca180db28ee937a280
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 17:41:29 +0300

    work with loop and if-else

--- main.c
+++ main.c
@@ -3,19 +3,21 @@
 int main() {
     int number;
 
-    /**
-     * вводим данные пользователя
-     */
-    printf("Введите число ");
-    scanf("%d", &number);
+    printf("Чтобы выйти из приложения введите -1\n\n");
 
-    if (number <= 10) {
-        printf("Ваше число меньше либо равно 10");
-    } else if (number <= 30) {
-        printf("Ваше число меньше либо равно 30");
-    } else {
-        printf("Ваше число больше 30");
+    while (1) {
+        printf("Введите число ");
+        scanf("%d", &number);
+
+        if (number < 0) {
+            printf("Выходим...\n");
+            return 0;
+        } else if (number <= 10) {
+            printf("Ваше число меньше либо равно 10\n");
+        } else if (number <= 30) {
+            printf("Ваше число меньше либо равно 30\n");
+        } else {
+            printf("Ваше число больше 30\n");
+        }
     }
-
-    return 0;
 }
