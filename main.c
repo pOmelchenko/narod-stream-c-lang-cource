5d0ca58a6cc88e34af0a1cfb45e90b7d0a36e160
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-04-17 16:20:19 +0300

    clean source

--- main.c
+++ main.c
@@ -1,17 +1,8 @@
-/**
- * Многострочкный комментарий
- * почему бы и нет..
- */
 #include <stdio.h>
 
 int main()
 {
-    char c1 = 10, c2 = 100, c_sub;
 
-    c_sub = c1 - c2;
-
-    // output
-    printf("Signed char: c2 = %hd; c2 = %hd\nc1 - c2 = %hd\n", c1, c2, c_sub);
 
     return 0;
 }
