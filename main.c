05c4db4d53390013bee6be6e1e46ec5db350bd3c
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-21 23:46:29 +0300

    implement fifth example

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n = 0;
+    char *strnum;
 
-    printf("Please enter an integer\n");
-    scanf("%d", &n);
-    printf(
-        "The number entered is %s10\n",
-        (n > 10)
-            ? "greater than "
-            : (n == 10)
-                ? ""
-                : "less than "
-    );
+    while ((n>=0) && (n<=10)) {
+        printf("Please enter an integer\n");
+        printf("To exit the program, enter number greater than 10 ...\n");
+        scanf("%d", &n);
+        strnum = n == 0 ? "zero" :
+                 n == 1 ? "one" :
+                 n == 2 ? "two" :
+                 n == 3 ? "three" :
+                 n == 4 ? "four" :
+                 n == 5 ? "five" :
+                 n == 6 ? "six" :
+                 n == 7 ? "seven" :
+                 n == 8 ? "eight" :
+                 n == 9 ? "nine" :
+                 n == 10 ? "ten" :
+                 "greater than 10 or less than 0";
+        printf("The number entered is %s.\n", strnum);
+    }
 
     return 0;
 }
