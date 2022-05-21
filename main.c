b8c81e2ffb332ccf59e6ebc2cc2fc768ceff6c10
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-21 23:33:19 +0300

    implement another example

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int n, m, res;
+    int n, res;
 
-    printf("Please enter an integer (n)\n");
+    printf("Please enter an integer\n");
     scanf("%d", &n);
-
-    printf("Please enter an integer (m)\n");
-    scanf("%d", &m);
-
-    res = (n>m) ? n : m;
-    printf("The maximum of entered: %d", res);
+    res = (n>=0) ? n : -n;
+    printf("The modulis of the entered number is %d", res);
 
     return 0;
 }
