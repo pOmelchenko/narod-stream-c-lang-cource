b4551febc4415227532ef6cf75c378f611232fd1
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-22 23:11:32 +0300

    the ninth try to work with type casting.

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 
-float my_div(float a, float b) {
-    return a / b;
-}
-
 int main() {
-    int arr_i[10] = {0,1,2,3,4,5,6,7,8,9};
-    float f = 3.1f;
-
-    // printf("Value is %d", arr_i[f]); // не рабочий вариант, так как f должен быть int
 
     return 0;
 }
