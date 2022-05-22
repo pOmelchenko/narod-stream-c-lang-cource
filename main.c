16cb014622c2e4eaf809b0ecffa9ea5c9ca5b612
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-22 22:51:52 +0300

    the first try to work with type casting

--- main.c
+++ main.c
@@ -1,43 +1,9 @@
 #include <stdio.h>
 
-#define VAR_CONST 12345
-#define HELLO_CONST "Hello"
-
 int main() {
-    const int i = 123;
-    printf("constant value is %d\n", 12);
-    printf("constant value is %d\n", i);
-    printf("constant value is %d\n", VAR_CONST);
-//    i = 12; // не возможно переопределить константу
-
-    // Работа с разными типами данных
-    printf("int\n");
-    printf("constant value is %ld\n", 234L);  // long
-    printf("constant value is %lu\n", 234UL); // unsigned long
-    printf("constant value is %lu\n", 234LU); // unsigned long
-    printf("constant value is %lu\n", 0xAABBCCDDEEFFLU); // unsigned long
-    printf("constant value is %llu\n", 0xFF00CCAAFFAABBCCULL); // unsigned long
-    printf("constant value is %llu\n", 0xFFFFFFFFFFFFFFFFULL); // unsigned long
-    printf("constant value is %lu\n", 07654321UL); // unsigned long
-
-    printf("double float\n");
-    printf("constant value is %.25lf\n", 4.321); // double
-    printf("constant value is %.25lf\n", 654321.321e-21); // double
-    printf("constant value is %.25f\n", 4.321f); // float
-    printf("constant value is %.25f\n", 654321.321e-21f); // float
-
-    printf("char\n");
-    printf("constant value is %c - 0x%02X\n", 'z', 'z'); // float
-    printf("constant value is %c - 0x%02X\n", '0', '0'); // float
-    printf("constant value is %c - 0x%02X\n", '\0', '\0'); // float
-    printf("constant value is %c - 0x%02X\n", '\n', '\n'); // float
-
-    printf("string\n");
-    const char str1[] = {"Hello!"};
-    printf("constant value is %s\n", HELLO_CONST); // string
-    printf("constant value is %s\n", str1); // string
-    printf("constant \\value\\ is %s\n", str1); // string
-    printf("constant \"value\" is %s\n", str1); // string
+    char c = 54;
+    int i = c;
+    printf("Value is %d", i);
 
     return 0;
 }
