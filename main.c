1ca37e8beb19bbe34a545fa0cd650db49125c89c
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-22 22:56:09 +0300

    the second try to work with type casting

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char c = 54;
-    int i = c;
-    printf("Value is %d", i);
+    int i = 1000000;
+    char c = i;
+    printf("Value is %d", c);
 
     return 0;
 }
