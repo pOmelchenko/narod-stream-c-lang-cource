e9f2564a6efce3778a1d1c66045328e1d13184d6
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-22 23:08:51 +0300

    the ninth try to work with type casting.

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ float my_div(float a, float b) {
 }
 
 int main() {
-    unsigned int i = 65535;
-    unsigned long long j = i;
+    int arr_i[10] = {0,1,2,3,4,5,6,7,8,9};
+    float f = 3.1f;
 
-    printf("Values is %u, %llu", i, j);
+    // printf("Value is %d", arr_i[f]); // не рабочий вариант, так как f должен быть int
 
     return 0;
 }
