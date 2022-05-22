856ce9fc16347df735a3549882a7e2616cc828b5
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-22 23:01:37 +0300

    the sixth try to work with type casting.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+float my_div(float a, float b) {
+    return a / b;
+}
+
 int main() {
     int i = 8;
     int j = 3;
-    float f_res = i / (float) j;
+    float f_res = my_div(i, j);
     printf("Result is %.10f", f_res);
 
     return 0;
