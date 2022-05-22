ee97a628e91ade8b82985376425d6bd30e6e5b7c
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-22 22:58:13 +0300

    the third try to work with type casting

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int i = 1000000;
-    char c = i;
-    printf("Value is %d", c);
+    int i = 8;
+    int j = 3;
+    float f_res = i/j;
+    printf("Result is %.10f", f_res);
 
     return 0;
 }
