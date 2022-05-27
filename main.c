ecc3973a275a8a4ae9f4791180f1984c39bda257
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-28 01:05:29 +0300

    implement `int_to_binary` function

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <strings.h>
 
-int main() {
+void int_to_binary(int, char*);
 
+int main(void) {
     return 0;
+}
+
+void int_to_binary(int x, char* in_str) {
+    char str_tmp[9] = {};
+    unsigned char i, j, k;
+    unsigned char bt[4] = {0};
+
+    for (j = 0; j < 4; j++) {
+        if (((x >> j*8) == 0) && (j > 0)) {
+            break;
+        }
+        bt[j] = x >> j*8;
+        strcpy(in_str, "0b");
+    }
+    for (k = 0; k < j; k++) {
+        for (i = 0; i < 8; i++) {
+            switch ((bt[j-k-1] >> i) & 0b00000001) {
+                case 1:
+                    str_tmp[7-1] = '1';
+                    break;
+                case 0:
+                    str_tmp[7-1] = '0';
+                    break;
+            }
+        }
+    }
+    strcat(in_str, str_tmp);
 }
