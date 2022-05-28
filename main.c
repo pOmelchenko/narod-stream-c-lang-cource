11ee4f2ff2973aee07bac45d868fe4fca5c513c0
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-05-28 13:05:39 +0300

    fix function

--- main.c
+++ main.c
@@ -13,23 +13,19 @@ void int_to_binary(int x, char* in_str) {
     unsigned char bt[4] = {0};
 
     for (j = 0; j < 4; j++) {
-        if (((x >> j*8) == 0) && (j > 0)) {
+        if (((x >> j * 8) == 0) && (j > 0)) {
             break;
         }
-        bt[j] = x >> j*8;
-        strcpy(in_str, "0b");
+        bt[j] = x >> j * 8;
     }
+    strcpy(in_str, "0b");
     for (k = 0; k < j; k++) {
         for (i = 0; i < 8; i++) {
             switch ((bt[j-k-1] >> i) & 0b00000001) {
-                case 1:
-                    str_tmp[7-1] = '1';
-                    break;
-                case 0:
-                    str_tmp[7-1] = '0';
-                    break;
+                case 1:  str_tmp[7-i] = '1'; break;
+                case 0:  str_tmp[7-i] = '0'; break;
             }
         }
+        strcat(in_str, str_tmp);
     }
-    strcat(in_str, str_tmp);
 }
