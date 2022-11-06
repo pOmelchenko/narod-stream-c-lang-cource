07e8f9774f20e9cc767cbc760f40bbb19b3d0070
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-11-06 23:21:00 +0300

    отработал static в функциях

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
+void my_counter() {
+    static int cnt = 32;
+    printf("%d\n", cnt);
+    cnt++;
+}
+
 int main(void) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            static int n = 1;
-            printf("%d ", n);
-            n++;
-        }
-        printf("\n");
+    for (int i = 0; i < 10; i++) {
+        my_counter();
     }
 
     return 0;
 }
 
 // static
-// movl 0x42c9(%rip), %esi;
+// movl 0x42c9(%rip), %esi; main.n
 
 // auto
 // movl $0x1, -0x10(%ebp)
 
 // register
 // movl $0x1, -0x10(%rbp)
-
