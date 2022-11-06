4c8ead8e9d5608b14aef6284ad0cd175f04cf462
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-11-06 14:08:00 +0300

    проверил попытку получить в `else` переменную которая была инициализирована в `if`

--- main.c
+++ main.c
@@ -12,5 +12,16 @@ int main(void) {
     int_to_binary(res, str1);
     printf("Value is %s\n", str1);
 
+    int aa = 10;
+    int bb = 5;
+
+    if (aa < bb) {
+        unsigned int rr = 42;
+        printf("n = %d\n", rr);
+    } else {
+        unsigned int rr = 10;
+        printf("n = %d\n", rr);
+    }
+
     return 0;
 }
